a2af0834ba3570f74eb955ba029c9b34f7f4253c
Tsenguunee <tsenguunborkhuu@gmail.com>
2024-05-12 21:33:02 +0800

    login

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -6,6 +6,14 @@ const users = [
     name: 'User',
     email: 'user@nextmail.com',
     password: '123456',
+    position: 'admin',
+  },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'User',
+    email: 'user1@nextmail.com',
+    password: '123456',
+    position: 'custom',
   },
 ];
 
